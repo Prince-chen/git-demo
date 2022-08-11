25749ca65ba5a5a7904622af6f61990a2e141908
Prince <Prince@world-tech.com.cn>
2022-08-11 16:37:50 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+
+/**
+ * @author 王子辰
+ * @data 2022/8/11 - 16:30
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
